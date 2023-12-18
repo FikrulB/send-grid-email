285ce4347ad517e543e11777e40c51447d4e21d4
FikrulB <mfikrulb@gmail.com>
2023-12-18 18:12:52 +0700

    send email

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package sendgridemail
 
-import "fmt"
+import (
+	"fmt"
+	"net/smtp"
+)
 
-func Send(message string) {
-	fmt.Println("Test => ", message)
+func SendMail(password string) {
+	auth := smtp.PlainAuth(
+		"",
+		"mfikrulb@gmail.com",
+		password,
+		"smtp.gmail.com",
+	)
+
+	msg := "Subject: This is a Subject\nThis is a Message"
+
+	err := smtp.SendMail(
+		"smtp.gmail.com:587",
+		auth,
+		"mfikrulb@gmail.com",
+		[]string{"mfikrulb@gmail.com"},
+		[]byte(msg),
+	)
+
+	if err != nil {
+		fmt.Println("Error ", err.Error())
+	}
 }
